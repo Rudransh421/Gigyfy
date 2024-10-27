ff6b7aa5d95a77d05a7e29d5cffc83a96275f089
Rudransh421 <rudransh7381@gmail.com>
2024-10-27 19:36:04 +0530

    initial setup completed

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
-import express from "express";
+import connectToDb from "./db/db.js";
 
+import { app } from "./app.js";
+
+import { config } from "dotenv";
+
+import "dotenv/config";
+
+config({ path: "./env" });
+
+app.on("error", (error) => {
+  console.log(`ERRR: ${error}`);
+  throw error;
+});
+
+connectToDb()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server running on port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Error appeared in hosting the server : ${err}`);
+  });
